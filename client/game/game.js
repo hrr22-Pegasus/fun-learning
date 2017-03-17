292e3167f2ad241460ffa9a3f14a356fc80c0149
Jesse Hill <jsnahill@gmail.com>
2017-03-16 20:38:10 -0700

    before hooking up database

--- client/game/game.js
+++ client/game/game.js
@@ -1,4 +1,100 @@
-angular.module('funLearning.game', [])
-  .controller('GameCtrl', function($scope){
+angular.module('funLearning.game', ['ngRoute'])
+  .controller('GameCtrl', function($scope, $interval, $http){
+    var interval;
+
+    $scope.highScore = 0;
+    $scope.timer = 10;
+    $scope.points = 0;
+
+    // $scope.getNameAgeGrade = function () {
+
+    // }
+
+
+
+    $scope.startGame = function(){
+      // $scope.timer = 10;
+      // $scope.points = 0;
+
+    }
+
+
+  $scope.setRandomNumbers = function(){
+      $scope.variable1 = Math.floor(Math.random() * 10);
+      $scope.variable2 = Math.floor(Math.random() * 10);
+      $scope.sum = $scope.variable1 + $scope.variable2;
+
+
+      interval = $interval(function(){
+        console.log("in interval", $scope.timer);
+        if($scope.timer > 0){
+          $scope.timer = $scope.timer - 1;
+        } else {
+          console.log("ending game");
+          $scope.endGame();
+        }
+      }, 1000);
+    };
+
+    $scope.startGame = function(){
+
+    }
+
+    $scope.endGame = function(){
+      console.log($scope.timer)
+      // $scope.timer = $scope.timer;
+      $interval.cancel(interval);
+      interval = undefined;
+      $scope.score =  $scope.points;
+      if ($scope.score > $scope.highScore){
+        $scope.highScore = $scope.score;
+      }
+      $scope.points = 0;
+    };
+
+    $scope.checkAnswer = function(input){
+      console.log("input", input);
+      if(input === $scope.sum){
+        console.log("input", input);
+        $scope.points += 1;
+        $scope.setRandomNumbers();
+      } else {
+        $scope.points -= 1;
+      }
+      $scope.input_sum = "";
+
+    };
+
+    $scope.submitPlayerInfo = function(data){
+
+      $http({
+        method: 'POST',
+        url: '/api/users',    //1234567890ag
+        data: data
+      }).then(function(){
+        console.log('post request succeeded! :)')
+      })
+    }
+
+    $scope.getPlayerHighScore = function(){
+
+      // $http({
+      //   method: GET;
+      //   url:  ;       //todo
+      // }).then(func tion(){
+
+      // })
+    }
+
+    $scope.getLeaderBoard = function(){
+
+    //   $http({
+    //     method: GET;
+    //     url:  ;       //todo
+    //   }).then(function(){
+
+    //   })
+    }
+
 
   })
--- server/server.js
+++ server/server.js
@@ -1,16 +1,67 @@
 var express = require('express');
+var User = require('../client/models/user.js');
 var bodyParser = require('body-parser');
 var path = require("path");
+var mongoose = require('mongoose');
+
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 console.log("directoy name: ", __dirname);
-
+mongoose.connect();
 var port = process.env.PORT || 1337;
 
 app.listen(port);
 console.log('Hey!');
 console.log("Listening on port: ", port);
+
+
+
+
+////////SERVER going to be routing///////////
+
+// var User = require('./app/models/user');
+
+// app.get("api/users", function(req, res){
+//   User.find({}, function(err, result){
+//     if (err) {
+//       console.log('Error fetching records', err);
+//     }
+//     res.status(200).send(result);
+//     console.log("Completed user get request")
+//   });
+// });
+
+app.post("/api/users", function(req, res){
+  console.log("Inside Post FUNCTION: @@@@@@@@@@@");
+  // var newUser = new User({
+  //   name: String,
+  //   age: Number,
+  //   grade: Number
+  // });
+
+  var userInfo = req.body;
+  var newUser = new User(userInfo);
+
+  newUser.save(function(err, result){
+  if (err) {
+    console.log('Error fetching records', err);
+  }
+    res.status(200).send(result);
+    console.log("user saved successfully")
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 module.exports = app;
