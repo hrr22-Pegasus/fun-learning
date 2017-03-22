4a04909cbec60d8d862f3b79e5326baabf5b9739
Ryan Perry <Rperry2174>
2017-03-22 15:05:16 -0400

    edited the gameplay

--- client/games/game.js
+++ client/games/game.js
@@ -7,15 +7,23 @@ angular.module('funLearning.game', [])
 
   GameState.Preload.prototype.getAvatar = function(){
     return UsersFactory.currentUser[0]["avatar"][0];
-  }
+  };
+
+  GameState.Level1.prototype.getTeacher = function(){
+    return UsersFactory.currentUser[0]["teacher"] //returns teacher to be used to get test
+  };
+
+  GameState.Level1.prototype.getTest = function(teacher){
+    return TestsFactory.getTest(teacher);
+  };
 
 
-  GameState.Level1.prototype.saveResults = function(results){
-    GameResultsFactory.set(results);
-  };
-  GameState.Level1.prototype.sendResults = function(){
-    GameResultsFactory.test();
-  };
+  // GameState.Level1.prototype.saveResults = function(results){
+  //   GameResultsFactory.set(results);
+  // };
+  // GameState.Level1.prototype.sendResults = function(){
+  //   GameResultsFactory.test();
+  // };
 
 
   console.log("GameState", GameState);
@@ -24,8 +32,6 @@ angular.module('funLearning.game', [])
   game.state.add('MainMenu', GameState.MainMenu);
   game.state.add('Level1', GameState.Level1);
 
-  var avatar = UsersFactory.currentUser[0]["avatar"][0]; //TODO
-  console.log(avatar, "user from game.js")
 
   game.state.start('Boot');
 
--- client/games/game1/states/level1.js
+++ client/games/game1/states/level1.js
@@ -7,6 +7,9 @@ GameState.Level1.prototype = {
     this.background = this.game.add.sprite(0, 0, 'background');
     this.kanye = new Player(this.game, 500, 350, 'kanye');
 
+    // this.teacher = this.getTeacher();
+    // this.test = this.getTest(this.teacher);
+
     this.test = [[0,1], [5,5], [3,3], [0,0]];
 
     this.textStyle = { font: '25px Desyrel', align: 'center'}
