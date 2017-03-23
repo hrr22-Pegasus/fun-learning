e6082f1b23a17e45dc9aa82d0cd76d3d3d184525
Jesse Hill <jsnahill@gmail.com>
2017-03-23 15:09:27 -0700

    Improve code readablity and fix style/spacing.

--- client/games/game.js
+++ client/games/game.js
@@ -7,46 +7,47 @@ angular.module('funLearning.game', [])
         console.log('all game data', GameResultsFactory.allGameData);
       });
     };
-    // var game = new Phaser.Game(600, 410, Phaser.CANVAS, 'game-canvas', null, false);
 
-    // GameState.Preload.prototype.getAvatar = function(){
+    var game = new Phaser.Game(600, 410, Phaser.CANVAS, 'game-canvas', null, false);
 
-    //   return UsersFactory.currentUser[0]["avatar"][0];
+    game.state.add('Boot', GameState.Boot);
+    game.state.add('Preload', GameState.Preload);
+    game.state.add('MainMenu', GameState.MainMenu);
+    game.state.add('Level1', GameState.Level1);
+    game.state.start('Boot');
+
+    GameState.Preload.prototype.getAvatar = function(){
+
+      return UsersFactory.currentUser[0]["avatar"][0];
+    };
+
+    // GameState.Level1.prototype.getTeacher = function(){
+    //   return UsersFactory.currentUser[0]["teacher"] //returns teacher to be used to get test
     // };
 
-  // // GameState.Level1.prototype.getTeacher = function(){
-  // //   return UsersFactory.currentUser[0]["teacher"] //returns teacher to be used to get test
-  // // };
+    // GameState.Level1.prototype.getTestByTeacher = function(){
+    //   return TestsFactory.currentTest[0]['games']['game1']['test1']
+    // };
 
-  // // GameState.Level1.prototype.getTestByTeacher = function(){
-  // //   return TestsFactory.currentTest[0]['games']['game1']['test1']
-  // // };
+    var game2 = new Phaser.Game(300, 300, Phaser.CANVAS, 'phaser-example', null, false);
 
+    // GameState2.Level1.prototype.getTeacher = function(){
+    //   return UsersFactory.currentUser[0]["teacher"] //returns teacher to be used to get test
+    // };
 
-  // console.log("GameState", GameState);
-  // game.state.add('Boot', GameState.Boot);
-  // game.state.add('Preload', GameState.Preload);
-  // game.state.add('MainMenu', GameState.MainMenu);
-  // game.state.add('Level1', GameState.Level1);
+    // GameState2.Level1.prototype.getTest = function(teacher){
+    //   return TestsFactory.getTest(teacher);
+    // };
 
-  // game.state.start('Boot');
+    GameState2.Level1.prototype.addResult = function(gameName, userName, data) {
+      console.log('game: ', gameName, 'user: ', userName, 'data: ', data);
+      return GameResultsFactory.addNewResult(gameName, userName, data);
+    }
 
-  var game2 = new Phaser.Game(300, 300, Phaser.CANVAS, 'phaser-example', null, false);
-  GameState2.Level1.prototype.getTeacher = function(){
-    return UsersFactory.currentUser[0]["teacher"] //returns teacher to be used to get test
-  };
+    game2.state.add('Boot', GameState2.Boot);
+    game2.state.add('Preload', GameState2.Preload);
+    game2.state.add('MainMenu', GameState2.MainMenu);
+    game2.state.add('Level1', GameState2.Level1);
+    game2.state.start('Boot');
 
-  GameState2.Level1.prototype.getTest = function(teacher){
-    return TestsFactory.getTest(teacher);
-  };
-  GameState2.Level1.prototype.addResult = function(gameName, userName, data) {
-    console.log('game: ', gameName, 'user: ', userName, 'data: ', data);
-    return GameResultsFactory.addNewResult(gameName, userName, data);
-  }
-  game2.state.add('Boot', GameState2.Boot);
-  game2.state.add('Preload', GameState2.Preload);
-  game2.state.add('MainMenu', GameState2.MainMenu);
-  game2.state.add('Level1', GameState2.Level1);
-
-  game2.state.start('Boot');
-});
+  });
--- client/services/gameResultsFactory.js
+++ client/services/gameResultsFactory.js
@@ -1,38 +1,37 @@
 angular.module('funLearning')
+  .factory('GameResultsFactory', function ($http) {
 
-.factory('GameResultsFactory', function ($http) {
+      var allGameData = [];
 
-    var allGameData = [];
+      var addNewResult = function(gameName, userName, data) {
+        return $http({
+          method: 'POST',
+          url: '/api/results/' + gameName + '/' + userName,
+          data: data
+        }).then(function(){
+          console.log('post request succeeded! :)')
+          getGameResults();
+        })
+      };
 
-    var addNewResult = function(gameName, userName, data) {
-      return $http({
-        method: 'POST',
-        url: '/api/results/' + gameName + '/' + userName,
-        data: data
-      }).then(function(){
-        console.log('post request succeeded! :)')
-        getGameResults();
-      })
-    };
+      var getGameResults = function() {
+        return $http({
+          method: 'GET',
+          url: '/api/results',
+        }).then(function(res){
+          console.log('get request succeeded! :)')
+          allGameData.push(res.data);
+          console.log(allGameData)
+          return res.data;
+        });
+      };
 
-    var getGameResults = function() {
-      return $http({
-        method: 'GET',
-        url: '/api/results',
-      }).then(function(res){
-        console.log('get request succeeded! :)')
-        allGameData.push(res.data);
-        console.log(allGameData)
-        return res.data;
-      });
-    };
-
-    return {
-      allGameData: allGameData,
-      addNewResult: addNewResult,
-      getGameResults: getGameResults
-    };
-});
+      return {
+        allGameData: allGameData,
+        addNewResult: addNewResult,
+        getGameResults: getGameResults
+      };
+  });
 
   // var gameResults = [];
 
